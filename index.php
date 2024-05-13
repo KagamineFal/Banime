ecec21e559aecaa48538778d8a0c822d11fa90e4
Kagamine Fal <akbar.falah.w@gmail.com>
2024-05-13 14:16:09 +0700

    Fal

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Banime</title>
-    <link rel="stylesheet" href="/Web.css?=V1">
+    <link rel="stylesheet" href="Web.css">
     <link rel="icon" href="/Assets/BanimeLogo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
