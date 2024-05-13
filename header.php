ee27ff23fb1a98dafe25a6ab6b1f6e4f7d1bc1a8
Kagamine Fal <akbar.falah.w@gmail.com>
2024-05-13 14:12:14 +0700

    Fal

---
+++ header.php
@@ -0,0 +1,43 @@
+<header>
+        <div class="header"></div>
+        <nav>
+            <img src="/Assets/Banime.png" alt="Profile" id="Logo">
+            <div class="list-menu" id="listMenu">
+                <i class="ph ph-list"></i>
+            </div>
+
+            <div class="navall navHidden" id="navList">
+                <div class="Nav">
+                    <a href="/"> HOME</a>
+                    <a href="/katagori.php"> CATEGORIES</a>
+                    <a href="/about.php"> ABOUT US</a>
+                </div>
+                <div class="Search">
+                    <form method="get" action="index.php">
+                        <input class="input hidden" type="text" name="search" id="input" placeholder="Search">
+                    </form>
+                    <i class="ph ph-magnifying-glass" id="search"></i>
+                </div>
+            </div>
+
+            <div class="Login">
+                <i class="ph ph-user-circle" onclick="toggleMenu()"></i>
+            </div>
+
+            <div class="sub-menu-warp" id="subMenu">
+                <div class="sub-menu">
+                    <div class="user-info">
+                        <i class="ph ph-user-circle"></i>
+                        <h2>Profile</h2>
+                    </div>
+                    <hr>
+
+                    <a href="/Admin/login.php" class="sub-menu-link">
+                        <i class="ph ph-key"></i>
+                        <p>Admin Area</p>
+                        <span>❯</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
